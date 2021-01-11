2c5ef35d57f549dc35b918b4edff0da538b6f3ef
Juan Pablo Villaplana <jpvillaplana@gmail.com>
2021-01-10 18:57:06 -0600

    Api respopnder

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponder;
+use Illuminate\Support\Facades\Request;
+
+class AuthorController extends Controller
+{
+    use ApiResponder;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($author)
+    {
+
+    }
+
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    public function destroy($author)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponder;
+use Illuminate\Support\Facades\Request;
+
+class BookController extends Controller
+{
+    use ApiResponder;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($book)
+    {
+
+    }
+
+    public function update(Request $request, $book)
+    {
+
+    }
+
+    public function destroy($book)
+    {
+
+    }
+}
