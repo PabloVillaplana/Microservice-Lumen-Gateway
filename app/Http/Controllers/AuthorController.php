9c22ed3afa7f925b8761b26b6ec06eb8cc8f4a63
Juan Pablo Villaplana <jpvillaplana@gmail.com>
2021-01-10 20:27:35 -0600

    checkpoint

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,44 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Traits\ApiResponder;
 use Illuminate\Support\Facades\Request;
 
 class AuthorController extends Controller
 {
     use ApiResponder;
+
+    public $authorService;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(AuthorService $authorService)
     {
-        //
+        $this->authorService = $authorService;
     }
 
     public function index()
     {
-
+        return $this->successResponse($this->authorService->obtainAuthors());
     }
 
     public function store(Request $request)
     {
-
+        return $this->successResponse($this->authorService->createAuthor($request->all(), Response::HTTP_CREATED));
     }
 
     public function show($author)
     {
-
+        return $this->successResponse($this->authorService->obtainAuthor($author));
     }
 
     public function update(Request $request, $author)
     {
-
+        return $this->successResponse($this->authorService->editAuthor($request->all(), $author));
     }
 
     public function destroy($author)
     {
-
+        return $this->successResponse($this->authorService->deleteAuthor($author));
     }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,44 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
+use App\Services\BookService;
 use App\Traits\ApiResponder;
 use Illuminate\Support\Facades\Request;
 
 class BookController extends Controller
 {
     use ApiResponder;
+    public $bookService;
+    public $authorService;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
-        //
+        $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     public function index()
     {
-
+        return $this->successResponse($this->bookService->obtainBooks());
     }
 
     public function store(Request $request)
     {
+        $this->authorService->obtainAuthor($request->author_id);
 
+        return $this->successResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
     public function show($book)
     {
-
+        return $this->successResponse($this->bookService->obtainBook($book));
     }
 
     public function update(Request $request, $book)
     {
-
+        return $this->successResponse($this->bookService->editBook($request->all(), $book));
     }
 
     public function destroy($book)
     {
-
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
 }
